5766c810afdfde9b0732d18b1df7081d64e2c211
Harjappan Singh <singhharjappan@gmail.com>
2024-05-06 14:41:29 +0100

    added toggle state to mark as completed

--- routes/web.php
+++ routes/web.php
@@ -52,3 +52,8 @@ Route::delete('/tasks/{task}', function (Task $task) {
     return redirect()->route('tasks.index')
         ->with('success', 'Task deleted successfully!');
 })->name('tasks.destroy');
+
+Route::put('tasks/{task}/toggle-complete', function (Task $task) {
+    $task->toggleComplete();
+    return redirect()->back()->with('success', 'Task update successfully!');
+})->name('tasks.toggle-complete');
